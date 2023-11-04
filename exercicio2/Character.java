6714f85ae8720eefb47ff0ca642cf83e44ae8420
TiagoSilva35 <tjcsilva04@gmail.com>
2023-11-04 22:07:39 +0000

    sasa

--- exercicio2/Character.java
+++ exercicio2/Character.java
@@ -21,15 +21,27 @@ class Character{
      */
     //getters and setters
     public String getName(){return name;}
+
     public int getExperience(){return experience;}
+
     public double getStrength(){return strength;}
+
     public double getInteligence(){return inteligence;}
+
     public double getAgility(){return agility;}
+
     public void setName(String name){this.name = name;}
+
     public void setStrength(int strength){this.strength = strength;}
+
     public void setInteligence(int inteligence){this.inteligence = inteligence;}
+
     public void setAgility(int agility){this.agility = agility;}
 
+    
+    /** 
+     * @param state
+     */
     //auxiliary function to print stats before and after levelling up
     protected void printStats(String state){
         System.out.println("-------- " + state + " -------");
@@ -46,6 +58,11 @@ class Character{
     protected void levelUp(){
     }
 
+    
+    /** 
+     * @param toString(
+     * @return boolean
+     */
     //method to check if a mercenary has an arch, a warrior has armour and a mage "sementes de abóbora"
     protected boolean hasAtribute(){return false;}
     
